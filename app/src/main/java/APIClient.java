c2e5a4bddf3ca746dffc37def29dd2a77e25a207
Zach Vega-Perkins <zachv@wepay.com>
2015-07-17 12:40:04 -0700

    Added get and post methods to the client.

--- app/src/main/java/APIClient.java
+++ app/src/main/java/APIClient.java
@@ -5,5 +5,24 @@
 import com.loopj.android.http.*;
 
 public class APIClient {
+    private static final String URL_BASE = "http://wecrowd.wepay.com/api/";
 
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url,
+                           RequestParams params,
+                           AsyncHttpResponseHandler responseHandler)
+    {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url,
+                            RequestParams params,
+                            AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return URL_BASE + relativeUrl;
+    }
 }
