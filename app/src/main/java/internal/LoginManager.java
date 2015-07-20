706c14f2601d7c546204e4c70fdbbdca7541fdf4
Zach Vega-Perkins <zachv@wepay.com>
2015-07-20 14:15:41 -0700

    Added logic for user type to the manager class.

--- app/src/main/java/internal/LoginManager.java
+++ app/src/main/java/internal/LoginManager.java
@@ -9,7 +9,13 @@ import com.loopj.android.http.RequestParams;
 import com.wepay.wecrowd.wecrowd.R;
 import android.content.Context;
 
+import org.apache.http.Header;
+import org.json.JSONObject;
+
 public class LoginManager {
+    public enum UserType { MERCHANT, PAYER };
+    public static UserType userType = UserType.PAYER;
+
     public static void loginFromContext(Context context,
                                         String email,
                                         String password,
@@ -21,6 +27,21 @@ public class LoginManager {
         params.put(context.getString(R.string.api_email_key), email);
         params.put(context.getString(R.string.api_password_key), password);
 
-        APIClient.post(context.getString(R.string.api_login_endpoint), params, handler);
+        APIClient.post(context.getString(R.string.api_login_endpoint), params, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        super.onSuccess(statusCode, headers, response);
+
+                        userType = UserType.MERCHANT;
+
+                        handler.onSuccess(statusCode, headers, response);
+                    }
+                }
+        );
+    }
+
+    public static void logout() {
+        // Simulate logout since no actual /logout API call exists
+        userType = UserType.PAYER;
     }
 }
