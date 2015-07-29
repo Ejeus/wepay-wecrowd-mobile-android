d9f6523150561c9496c24aa56cdf409a6ca62aee
Zach Vega-Perkins <zachv@wepay.com>
2015-07-29 14:05:38 -0700

    Completed view setup.

--- app/src/main/java/com/wepay/wecrowd/wecrowd/SwipePaymentActivity.java
+++ app/src/main/java/com/wepay/wecrowd/wecrowd/SwipePaymentActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class SwipePaymentActivity extends ActionBarActivity {
@@ -12,6 +15,21 @@ public class SwipePaymentActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_payment);
+
+        setUpViewInformation();
     }
 
+
+    private void setUpViewInformation() {
+        ViewGroup donationField;
+        TextView donateTag;
+        EditText donateEditText;
+
+        donationField = (ViewGroup) findViewById(R.id.swiper_payment_donation_field);
+        donateTag = (TextView) donationField.getChildAt(0);
+        donateEditText = (EditText) donationField.getChildAt(1);
+
+        donateTag.setText(getString(R.string.title_donation_manual));
+        donateEditText.setText(getString(R.string.demo_payer_donation_amount));
+    }
 }
