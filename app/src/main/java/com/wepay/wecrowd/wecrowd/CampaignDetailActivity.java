f7cbf0f542b1d7ac74188acf0354d2a1d0f986be
Zach Vega-Perkins <zachv@wepay.com>
2015-07-29 13:17:15 -0700

    Created basic donation option menu.

--- app/src/main/java/com/wepay/wecrowd/wecrowd/CampaignDetailActivity.java
+++ app/src/main/java/com/wepay/wecrowd/wecrowd/CampaignDetailActivity.java
@@ -5,11 +5,15 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -33,35 +37,17 @@ public class CampaignDetailActivity extends AppCompatActivity {
         setUpCampaignDetail();
         configureViewForUserType(LoginManager.userType);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_campaign_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     
     public void didSelectDonate(View view) {
         if (LoginManager.userType == LoginManager.UserType.PAYER) {
-            startActivity(new Intent(this, ManualPaymentActivity.class));
+//            startActivity(new Intent(this, ManualPaymentActivity.class));
         } else if (LoginManager.userType == LoginManager.UserType.MERCHANT) {
             // TODO: Show payment options for merchant
         }
+
+        PopupMenu menu = new PopupMenu(this, findViewById(R.id.campaign_detail_button_donate));
+        menu.inflate(R.menu.menu_donation);
+        menu.show();
     }
 
     private void setUpCampaignDetail() {
