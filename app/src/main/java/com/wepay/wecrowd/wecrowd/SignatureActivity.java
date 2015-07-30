b7bc06ff42171708e84faa9fe00a2f43672977e5
Zach Vega-Perkins <zachv@wepay.com>
2015-07-30 10:16:02 -0700

    Added clear signature action.

--- app/src/main/java/com/wepay/wecrowd/wecrowd/SignatureActivity.java
+++ app/src/main/java/com/wepay/wecrowd/wecrowd/SignatureActivity.java
@@ -2,15 +2,45 @@ package com.wepay.wecrowd.wecrowd;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 
 public class SignatureActivity extends AppCompatActivity {
+    SignaturePad signaturePad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signature);
+
+        signaturePad = (SignaturePad) findViewById(R.id.signature_pad);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_signature, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear) {
+            signaturePad.clear();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
